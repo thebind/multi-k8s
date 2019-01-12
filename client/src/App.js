31ae28a702478e99f4afb94d6b4762fb57cda5c0
thebind <takeo@thebind.io>
2019-01-12 15:25:06 +0900

    updated second take

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
-
-import Fib from './Fib';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 class App extends Component {
   render() {
@@ -14,14 +12,13 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version Kubernetes 3</h1>
+            <h1 className="App-title">Fib Calculator version Kubernetes 3.0</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} 
-            />
+            <Route path="/otherpage" component={OtherPage} />
           </div>
         </div>
       </Router>
